fe688713e124e31b8945d2b83702271b378a4add
Danny Lee <dannyworkingraw@gmail.com>
2019-04-12 17:47:09 -0400

    added basic javascript to display photos with api

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-function getJSONData() {
-    var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback?";
-    $.getjSON(flickrAPI, {
-            id: "165128840@N04",
-            format: "json",
-        },
-        doneFn
-    );
-}
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=0cd6f618bad7076ac88fc8f7f1b5cbdb&user_id=165128840@N04&format=json&nojsoncallback=1",
+	"method": "GET",
+	"headers": {}
+  }
+  
+  $.ajax(settings).done(function (data) {
+	console.log(data);
+	
 
-function doneFn (result){
-    for (var i=0; i<result.items.length; i++){
-        console.log(result.items[i].link);
-        var img = $('<img>');
-        image.attr('src', result.items[i].media.m);
-        image.attr('height', '100');
-        image.attr('padding', '10px');
-        $('#content').append(image);
-    }
-}
+$("#galleryTitle").append(data.photos.photo[0].title + " Gallery");
+$.each( data.photos.photo, function( i, gp ) {
 
-$(document).ready(function(){
-    getJSONData();
+var farmId = gp.farm;
+var serverId = gp.server;
+var id = gp.id;
+var secret = gp.secret;
+
+console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
+
+//  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+$("#content").append('<img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/>');
+
+});
 });
